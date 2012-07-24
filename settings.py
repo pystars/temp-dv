cfcf9ff15d57b63a805575b5bd4468e7f5f46e2c
clincher <va.bolshakov@gmail.com>
2012-07-24 19:27:22 +0400

    cache prefix key added

--- settings.py
+++ settings.py
@@ -28,6 +28,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
         'LOCATION': '127.0.0.1:11211',
+        'KEY_PREFIX': 'temp-dv_'
     }
 }
 #CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
